ee9219eedb76c40093ac42a25099d5160d60cccd
Kasey <kaseyjameshinton@gmail.com>
2019-11-07 23:31:39 -0700

    Modifies styles. Sets up table. Populates with some fake data.

--- src/components/car-list/car-list.ts
+++ src/components/car-list/car-list.ts
@@ -1,6 +1,8 @@
-import { LitElement, customElement, html, css } from 'lit-element';
-import { displayFlex, selfCenter, vertical } from '@collaborne/lit-flexbox-literals';
+import { LitElement, customElement, html, css, property } from 'lit-element';
+import { displayFlex, vertical } from '@collaborne/lit-flexbox-literals';
 import { CSTBannerSingleton } from '../cst-banner/cst-banner';
+
+let firstLoad: boolean = false;
 
 @customElement('car-list')
 export default class CarListElement extends LitElement {
@@ -8,23 +10,67 @@ export default class CarListElement extends LitElement {
     :host {
       ${displayFlex}
       ${vertical}
+      margin: 16px 0;
     }
 
-    h1 {
-      ${displayFlex}
-      ${selfCenter}
+    img {
+      margin-top: 24px;
+      height: 132px;
+    }
+
+    th {
+      text-align: left;
+    }
+
+    tr:nth-child(2n) {
+      background-color: #f9f9f9;
+    }
+
+    td:hover {
+      cursor: default;
     }
   `;
 
   firstUpdated() {
-    setTimeout(() => {
-      CSTBannerSingleton.show('Welcome!', 'Thanks for trying out the car service tracker. We use cookies!', 'Thanks!', '');
-    }, 500);
+    if (!firstLoad) {
+      firstLoad = true;
+      CSTBannerSingleton.show('Thanks!', 'We appreciate you trying our car service tracker.', 'Ok', '');
+    }
   }
 
   render() {
     return html`
-      <h1>Car List</h1>
+      <link href="https://unpkg.com/@amber-ds/visual@1.0.1/dist/index.css" rel="stylesheet" />
+      <h2>Vehicle Inventory</h2>
+      <table>
+        <tr>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Year</th>
+        </tr>
+        <tr>
+          <td>Mitsubishi</td>
+          <td>Mighty Max</td>
+          <td>1994</td>
+        </tr>
+        <tr>
+          <td>Chevrolet</td>
+          <td>Equinox</td>
+          <td>2016</td>
+        </tr>
+        <tr>
+          <td>Chevrolet</td>
+          <td>Malibu</td>
+          <td>2004</td>
+        </tr>
+        <tr>
+          <td>Ford</td>
+          <td>Expedition</td>
+          <td>1998</td>
+        </tr>
+      </table>
+
+      <img src="images/undraw_fast_car.svg" />
     `;
   }
 }
