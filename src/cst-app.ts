6b0ae2948203242e6e53454a4b754794c70c3cac
Kasey <kaseyjameshinton@gmail.com>
2019-11-08 16:36:27 -0700

    Uses justin fagnani's approach to lazy loading and using page js

--- src/cst-app.ts
+++ src/cst-app.ts
@@ -2,55 +2,81 @@
 import page from 'page';
 import './components/cst-snackbar/cst-snackbar';
 import './components/cst-header/cst-header';
-import './components/cst-404/cst-404';
 import './components/cst-loading/cst-loading';
 
 import { NavigateEvent } from './utilities/events';
 import CSTStyles from './styles/cst-styles/cst-styles';
+import lazyLoad from './utilities/lazy-load';
 
 @customElement('cst-app')
 export class CSTAppElement extends LitElement {
   @property({ type: Boolean }) isLoading: boolean = true;
   @property({ type: String }) page: string | undefined;
+  @property({ type: String }) currentView: 'vehicles' | 'vehicle' | 'not-found' = 'vehicles';
+  @property({ type: Number }) vehicleId: number;
 
-  firstUpdated() {
+  constructor() {
+    super();
+    this._installRoutes();
     this.addEventListener(NavigateEvent.eventName, (event: NavigateEvent) => page.show(event.detail));
-
-    page('/', () => {
-      this.pageChanged('/');
+    window.addEventListener('lazy-load-complete', () => {
+      this.isLoading = false;
     });
-
-    page('*', () => {
-      this.pageChanged('404');
-    });
-
-    page.start();
   }
 
-  async pageChanged(mainPage: string) {
-    this.page = mainPage;
-    this.isLoading = true;
+  private _installRoutes() {
+    page.redirect('/', '/vehicles');
+    page('/vehicles', this._vehiclesRoute.bind(this));
+    page('/vehicle/:id', this._vehicleRoute.bind(this));
+    page('*', this._notFoundRoute.bind(this));
+    page();
+  }
 
-    try {
-      switch (mainPage) {
-        case '/':
-          await import('./components/cst-list/cst-list.js');
-          break;
-      }
-      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-    } catch (error) {
-      console.warn(error);
-      this.pageChanged('404');
-    } finally {
-      setTimeout(() => {
-        this.isLoading = false;
-      }, 300);
+  private _vehiclesRoute() {
+    this.isLoading = true;
+    this.currentView = 'vehicles';
+  }
+
+  private _vehicleRoute(context) {
+    this.isLoading = true;
+    this.currentView = 'vehicle';
+    const vehicleId: string = context.params['id'];
+    this.vehicleId = Number(vehicleId);
+  }
+
+  private _notFoundRoute() {
+    this.isLoading = true;
+    this.currentView = 'not-found';
+  }
+
+  private _renderCurrentView() {
+    switch (this.currentView) {
+      case 'vehicles':
+        return lazyLoad(
+          import('./components/cst-list/cst-list.js'),
+          html`
+            <cst-list></cst-list>
+          `
+        );
+      case 'vehicle':
+        return lazyLoad(
+          import('./components/cst-vehicle/cst-vehicle.js'),
+          html`
+            <cst-vehicle .vehicleId=${this.vehicleId}></cst-vehicle>
+          `
+        );
+      case 'not-found':
+        return lazyLoad(
+          import('./components/cst-404/cst-404.js'),
+          html`
+            <cst-404></cst-404>
+          `
+        );
     }
   }
 
   static styles = css`
-    ${CSTStyles}
-    :host {
+    ${CSTStyles} :host {
       --app-primary-color: #86addb;
       --app-secondary-color: #b8b8b8;
       --app-text-color: #5c5c5c;
@@ -65,11 +91,16 @@ export class CSTAppElement extends LitElement {
       margin: 0 auto;
       max-width: 900px;
       width: 100%;
+      background: #fff;
     }
 
     cst-loading {
       align-self: center;
       margin-top: 24px;
+    }
+
+    [hidden] {
+      display: none;
     }
   `;
 
@@ -78,16 +109,11 @@ export class CSTAppElement extends LitElement {
       <main class="wrapper">
         <cst-header></cst-header>
         <main-content>
-          ${this.isLoading
-            ? html`
-                <cst-loading></cst-loading>
-              `
-            : html`
-                <cst-list ?hidden=${this.page !== '/'} ?isActive=${this.page === '/'}></cst-list>
-                <cst-404 ?hidden=${this.page !== '404'}></cst-404>
-              `}
+          <cst-loading ?hidden=${!this.isLoading}></cst-loading>
+          <div ?hidden=${this.isLoading}>
+            ${this._renderCurrentView()}
+          </div>
         </main-content>
-
         <cst-snackbar></cst-snackbar>
       </main>
     `;
